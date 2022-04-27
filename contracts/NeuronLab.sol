6760bbb39e94173e02c812ebc24cd780f68af4b5
Jadedvi104 <jadedvi104@gmail.com>
2022-04-27 14:13:23 +0700

    updates done by Jirayu

--- contracts/NeuronLab.sol
+++ contracts/NeuronLab.sol
@@ -6,16 +6,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IECIONFT {
     function tokenInfo(uint256 _tokenId)
         external
         view
         returns (string memory, uint256);
+
+    function ownerOf(uint256 _tokenId) external view returns (address);
+
+    function burn(uint256 _tokenId) external;
+
+    function safeMint(address _to, string memory partCode) external;
 }
 
 contract NeuronLab is Ownable {
-    // Part Code Index
+    using Strings for string;
+    //Part Code Index
     uint256 constant PC_NFT_TYPE = 12;
     uint256 constant PC_KINGDOM = 11;
     uint256 constant PC_CAMP = 10;
@@ -30,6 +38,14 @@ contract NeuronLab is Ownable {
     uint256 constant PC_RESERVED1 = 1;
     uint256 constant PC_RESERVED2 = 0;
 
+    //Genom Rarity Code
+    uint32 constant GENOME_COMMON = 0;
+    uint32 constant GENOME_RARE = 1;
+    uint32 constant GENOME_EPIC = 2;
+    uint32 constant GENOME_LEGENDARY = 3;
+    uint32 constant GENOME_LIMITED = 4;
+
+    //Stars tier
     string constant ZERO_STAR = "00";
     string constant ONE_STAR = "01";
     string constant TWO_STAR = "02";
@@ -37,21 +53,74 @@ contract NeuronLab is Ownable {
     string constant FOUR_STAR = "04";
     string constant FIVE_STAR = "05";
 
+    uint256 public rate;
+
+    //Mapping to check Genom Rarity
+    mapping(string => uint32) public genomRarity;
+
     IECIONFT public NFTCore;
     IERC20 public ECIO_TOKEN;
 
-    function checkUserRarity(string memory partCode)
+    //Setup ECIO Token Address
+    function setupEcioToken(address ecioTokenAddr) public onlyOwner {
+        ECIO_TOKEN = IERC20(ecioTokenAddr);
+    }
+
+    //Setup NFTcore address
+    function setupNFTCore(IECIONFT nftCore) public onlyOwner {
+        NFTCore = nftCore;
+    }
+
+    //Setup NFTcore address
+    function setupRate(uint256 newRate) public onlyOwner {
+        rate = newRate;
+    }
+
+    //Compare 2 strings
+    function compareStrings(string memory a, string memory b)
+        public
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked((a))) ==
+            keccak256(abi.encodePacked((b))));
+    }
+
+    //Get user Partcode and then split the code to check Genomic numbers
+    function splitGenom(string memory partCode)
         public
         pure
         returns (string memory)
     {
         string[] memory splittedPartCodes = splitPartCode(partCode);
-        string memory nftType = splittedPartCodes[PC_NFT_TYPE];
+        string memory genType = splittedPartCodes[PC_GENOME];
 
-        return (nftType);
+        return (genType);
     }
 
-    function checkUserStars(string memory partCode)
+    //Get user Genomic Partcode and then split the code to check Genomic Rarity
+    function checkUserGenomRarity(string memory genomPart)
+        public
+        pure
+        returns (uint32)
+    {
+        if (genomRarity.genomPart == GENOME_COMMON) {
+            return GENOME_COMMON;
+        } else if (genomRarity.genomPart == GENOME_RARE) {
+            return GENOME_RARE;
+        } else if (genomRarity.genomPart == GENOME_EPIC) {
+            return GENOME_EPIC;
+        } else if (genomRarity.genomPart == GENOME_LEGENDARY) {
+            return GENOME_LEGENDARY;
+        } else if (genomRarity.genomPart == GENOME_LIMITED) {
+            return GENOME_LIMITED;
+        } else {
+            return 999; // need to change this
+        }
+    }
+
+    //Get user Partcode and then split the code to check stars numbers
+    function splitPartcodeStar(string memory partCode)
         public
         pure
         returns (string memory)
@@ -62,6 +131,7 @@ contract NeuronLab is Ownable {
         return (starCode);
     }
 
+    //Split partcode for each part
     function splitPartCode(string memory partCode)
         public
         pure
@@ -79,52 +149,187 @@ contract NeuronLab is Ownable {
         return result;
     }
 
-    function compareStrings(string memory a, string memory b)
+    //Combine partcode
+    function createPartCode(
+        string memory equipmentCode,
+        string memory starCode,
+        string memory weapCode,
+        string memory humanGENCode,
+        string memory battleBotCode,
+        string memory battleSuiteCode,
+        string memory battleDROCode,
+        string memory battleGearCode,
+        string memory trainingCode,
+        string memory kingdomCode,
+        string memory nftTypeCode
+    ) public pure returns (string memory) {
+        string memory code = concateCode("", "00");
+        code = concateCode(code, "00");
+        code = concateCode(code, equipmentCode);
+        code = concateCode(code, starCode);
+        code = concateCode(code, weapCode);
+        code = concateCode(code, humanGENCode);
+        code = concateCode(code, battleBotCode);
+        code = concateCode(code, battleSuiteCode);
+        code = concateCode(code, battleDROCode);
+        code = concateCode(code, battleGearCode);
+        code = concateCode(code, trainingCode); //Reserved
+        code = concateCode(code, kingdomCode); //Reserved
+        code = concateCode(code, nftTypeCode); //Reserved
+        return code;
+    }
+
+    function concateCode(string memory concatedCode, string memory newCode)
         public
         pure
-        returns (bool)
+        returns (string memory)
     {
-        return (keccak256(abi.encodePacked((a))) ==
-            keccak256(abi.encodePacked((b))));
+        concatedCode = string(abi.encodePacked(concatedCode, newCode));
+
+        return concatedCode;
     }
 
-    function compareStar(uint256 tokenIds) public view returns (string memory) {
-        string memory partCode;
-        uint256 id;
-        (partCode, id) = NFTCore.tokenInfo(tokenIds);
+    //Get Card id and then burn them and mint a new one
+    function gatherMaterials(uint256[] memory tokenIds, uint256 mainCardTokenId)
+        external
+    {
+        require(
+            ECIO_TOKEN.balanceOf(msg.sender) >= rate,
+            "Token: your token is not enough"
+        );
 
-        if (compareStrings(ONE_STAR, checkUserStars(partCode)) == true) {
-            return ONE_STAR;
-        }
-        
-        if (compareStrings(TWO_STAR, checkUserStars(partCode)) == true) {
-            return TWO_STAR;
+        string memory mainCardPart;
+        (mainCardPart, ) = NFTCore.tokenInfo(mainCardTokenId);
+        string memory mainCardGenom = splitGenom(mainCardPart);
+
+        for (uint32 i = 0; i < tokenIds.length; i++) {
+            string memory tokenIdPart;
+            (tokenIdPart, ) = NFTCore.tokenInfo(tokenIds[i]);
+            require(
+                NFTCore.ownerOf(tokenIds[i]) == msg.sender,
+                "Ownership: you are not the owner"
+            );
+            require(
+                compareStrings(mainCardGenom, tokenIdPart) == true,
+                "Genomic: Genomic is not the same as main Card"
+            );
+
+            NFTCore.burn(tokenIds[i]);
         }
 
-        if (compareStrings(THREE_STAR, checkUserStars(partCode)) == true) {
-            return THREE_STAR;
-        }
-        if (compareStrings(FOUR_STAR, checkUserStars(partCode)) == true) {
-            return FOUR_STAR;
-        }
-        if (compareStrings(FIVE_STAR, checkUserStars(partCode)) == true) {
-            return FIVE_STAR;
-        }
+        //get to know main part code star
+        string memory mainCardStar = splitPartcodeStar(mainCardPart);
 
-        return ZERO_STAR;
+        // Upgrade from 0 Star to 1 star
+        if (compareStrings(mainCardStar, ZERO_STAR) == true) {
+            // split part code
+            string[] memory splittedPartCode = splitPartCode(mainCardPart);
+            // change part code
+            splittedPartCode[PC_STAR] = ONE_STAR;
+            // update partcode
+            string memory partCode = createPartCode(
+                splittedPartCode[PC_EQUIPMENT], //equipmentTypeId
+                splittedPartCode[PC_STAR], //combatStarCode
+                splittedPartCode[PC_WEAPON], //WEAPCode
+                splittedPartCode[PC_GENOME], //humanGENCode
+                splittedPartCode[PC_BOT], //battleBotCode
+                splittedPartCode[PC_SUITE], //battleSuiteCode
+                splittedPartCode[PC_DRONE], //battleDROCode
+                splittedPartCode[PC_GEAR], //battleGearCode
+                splittedPartCode[PC_CAMP], //trainingCode
+                splittedPartCode[PC_KINGDOM], //kingdomCode
+                splittedPartCode[PC_NFT_TYPE] // nft Type
+            );
+
+            NFTCore.safeMint(msg.sender, partCode);
+        } else if ( compareStrings(mainCardStar, ONE_STAR) == true) {
+            // split part code
+            string[] memory splittedPartCode = splitPartCode(mainCardPart);
+            // change part code
+            splittedPartCode[PC_STAR] = TWO_STAR;
+            // update partcode
+            string memory partCode = createPartCode(
+                splittedPartCode[PC_EQUIPMENT], //equipmentTypeId
+                splittedPartCode[PC_STAR], //combatStarCode
+                splittedPartCode[PC_WEAPON], //WEAPCode
+                splittedPartCode[PC_GENOME], //humanGENCode
+                splittedPartCode[PC_BOT], //battleBotCode
+                splittedPartCode[PC_SUITE], //battleSuiteCode
+                splittedPartCode[PC_DRONE], //battleDROCode
+                splittedPartCode[PC_GEAR], //battleGearCode
+                splittedPartCode[PC_CAMP], //trainingCode
+                splittedPartCode[PC_KINGDOM], //kingdomCode
+                splittedPartCode[PC_NFT_TYPE] // nft Type
+            );
+
+            NFTCore.safeMint(msg.sender, partCode);
+        } else if (compareStrings(mainCardStar, TWO_STAR) == true) {
+            // split part code
+            string[] memory splittedPartCode = splitPartCode(mainCardPart);
+            // change part code
+            splittedPartCode[PC_STAR] = THREE_STAR;
+            // update partcode
+            string memory partCode = createPartCode(
+                splittedPartCode[PC_EQUIPMENT], //equipmentTypeId
+                splittedPartCode[PC_STAR], //combatStarCode
+                splittedPartCode[PC_WEAPON], //WEAPCode
+                splittedPartCode[PC_GENOME], //humanGENCode
+                splittedPartCode[PC_BOT], //battleBotCode
+                splittedPartCode[PC_SUITE], //battleSuiteCode
+                splittedPartCode[PC_DRONE], //battleDROCode
+                splittedPartCode[PC_GEAR], //battleGearCode
+                splittedPartCode[PC_CAMP], //trainingCode
+                splittedPartCode[PC_KINGDOM], //kingdomCode
+                splittedPartCode[PC_NFT_TYPE] // nft Type
+            );
+
+            NFTCore.safeMint(msg.sender, partCode);
+        } else if (compareStrings(mainCardStar, THREE_STAR) == true) {
+            // split part code
+            string[] memory splittedPartCode = splitPartCode(mainCardPart);
+            // change part code
+            splittedPartCode[PC_STAR] = FOUR_STAR;
+            // update partcode
+            string memory partCode = createPartCode(
+                splittedPartCode[PC_EQUIPMENT], //equipmentTypeId
+                splittedPartCode[PC_STAR], //combatStarCode
+                splittedPartCode[PC_WEAPON], //WEAPCode
+                splittedPartCode[PC_GENOME], //humanGENCode
+                splittedPartCode[PC_BOT], //battleBotCode
+                splittedPartCode[PC_SUITE], //battleSuiteCode
+                splittedPartCode[PC_DRONE], //battleDROCode
+                splittedPartCode[PC_GEAR], //battleGearCode
+                splittedPartCode[PC_CAMP], //trainingCode
+                splittedPartCode[PC_KINGDOM], //kingdomCode
+                splittedPartCode[PC_NFT_TYPE] // nft Type
+            );
+
+            NFTCore.safeMint(msg.sender, partCode);
+        } else if (compareStrings(mainCardStar, FOUR_STAR) == true) {
+            // split part code
+            string[] memory splittedPartCode = splitPartCode(mainCardPart);
+            // change part code
+            splittedPartCode[PC_STAR] = FIVE_STAR;
+            // update partcode
+            string memory partCode = createPartCode(
+                splittedPartCode[PC_EQUIPMENT], //equipmentTypeId
+                splittedPartCode[PC_STAR], //combatStarCode
+                splittedPartCode[PC_WEAPON], //WEAPCode
+                splittedPartCode[PC_GENOME], //humanGENCode
+                splittedPartCode[PC_BOT], //battleBotCode
+                splittedPartCode[PC_SUITE], //battleSuiteCode
+                splittedPartCode[PC_DRONE], //battleDROCode
+                splittedPartCode[PC_GEAR], //battleGearCode
+                splittedPartCode[PC_CAMP], //trainingCode
+                splittedPartCode[PC_KINGDOM], //kingdomCode
+                splittedPartCode[PC_NFT_TYPE] // nft Type
+            );
+
+            NFTCore.safeMint(msg.sender, partCode);
+        }
     }
 
-    function calculatePercent() public view returns (uint256) {}
-
-    function addSwToForge() public {}
-
-    function gatherMaterials(uint256[] memory tokenIds)
-        internal
-        view
-        returns (uint256[] memory)
-    {}
-
-    function upgradeTier(uint256 tokenId) external {}
+    //*************************** transfer fee ***************************//
 
     function transferFee(address payable _to, uint256 _amount)
         public
